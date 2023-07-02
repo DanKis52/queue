a9e07c9fddf21ac0f2cb54edfa9b77743330485f
Dan <kiselev.danil201313@gmail.com>
2023-07-02 21:10:58 +0300

    12.12

--- queue.cpp
+++ queue.cpp
@@ -1,9 +1,89 @@
-﻿// queue.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "queue.h"
 
-#include <iostream>
+const int MIN_PER_HR = 60;
+
+bool newCustomer(double x);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	using std::cin;
+	using std::cout;
+	using std::endl;
+	using std::ios_base;
+	
+	std::srand(std::time(0));
+	cout << "Case Study: Bank of Heather Automatic Teller\n";
+	cout << "Enter maximum size of queue: ";
+	int qs;
+	cin >> qs;
+	Queue line(qs);
+	cout << "Enter a number of simulation hours: ";
+	int hours;
+	cin >> hours;
+	long cycleLimit = MIN_PER_HR * hours;
+	cout << "Enter the average number of customers per hour: ";
+	double perhour;
+	cin >> perhour;
+	double min_per_cust = MIN_PER_HR / perhour;
+
+	Item temp;
+	long turnaways = 0;
+	long customers = 0;
+	long served = 0;
+	long sum_line = 0;
+	int wait_time = 0;
+	long line_wait = 0;
+
+	for (int cycle = 0; cycle < cycleLimit; cycle++)
+	{
+		if (newCustomer(min_per_cust))
+		{
+			if (line.isFull())
+			{
+				turnaways++;
+			}
+			else
+			{
+				customers++;
+				temp.set(cycle);
+				line.enQueue(temp);
+			}
+		}
+		if (wait_time<=0 && !line.isEmpty())
+		{
+			line.deQueue(temp);
+			wait_time = temp.ptime();
+			line_wait += cycle - temp.when();
+			served++;
+		}
+		if (wait_time > 0)
+		{
+			wait_time--;
+		}
+		sum_line += line.queueCount();
+	}
+	if (customers > 0)
+	{
+		cout << "customers accepted: " << customers << endl;
+		cout << "customers served: " << served << endl;
+		cout << "turnaways: " << turnaways << endl;
+		cout << "average queue size: ";
+		cout.precision(2);
+		cout.setf(ios_base::fixed, ios_base::floatfield);
+		cout << (double)sum_line / cycleLimit << endl;
+		cout << "average wait time: " << (double)line_wait / served << endl;
+	}
+	else
+	{
+		cout << "No customers!\n";
+	}
+	cout << "Done!\n";
+	return 0;
+}
+
+bool newCustomer(double x) {
+	return (std::rand() * x / RAND_MAX < 1);
 }
--- queue.h
+++ queue.h
@@ -1,1 +1,43 @@
 #pragma once
+class Customer
+{
+public:
+	Customer() { arrive = processTime = 0; }
+	void set(long when);
+	long when() const { return arrive; }
+	int ptime() const { return processTime; }
+private:
+	long arrive;
+	int processTime;
+};
+
+typedef Customer Item;
+
+class Queue
+{
+public:
+	Queue(int qs = Q_SIZE);
+	~Queue();
+	bool isEmpty() const;
+	bool isFull() const;
+	int queueCount() const;
+	bool enQueue(const Item& item);
+	bool deQueue(Item& item);
+
+private:
+	enum {Q_SIZE = 10};
+	struct Node
+	{
+		Item item;
+		struct Node* next;
+	};
+
+	Node* front;
+	Node* rear;
+	int items;
+	const int qsize;
+
+	// ”преждающие объ€влени€ дл€ предотвращени€ открытого копировани€
+	//Queue(const Queue& q) : qsize(0) {}
+	Queue& operator=(const Queue& q) { return *this; }
+};
